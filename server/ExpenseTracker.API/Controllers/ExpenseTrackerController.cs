b21219d51a294247b61f82b61b0215c25fa2fb41
Daniele Manna <danielemanna02@gmail.com>
2025-04-20 13:35:08 +0200

    init

---
+++ server/ExpenseTracker.API/Controllers/ExpenseTrackerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.API.Controllers;
+
+[Route("api/[controller]")]
+public class ExpenseTrackerController : Controller
+{
+    #region GET
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("pong");
+    }
+
+    [HttpGet("expenses")]
+    public IActionResult GetExpenses()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion GET
+
+    #region POST
+
+    [HttpPost("expenses")]
+    public IActionResult PostExpenses()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+    
+    #region PUT
+
+    [HttpPut("expenses")]
+    public IActionResult PutExpenses()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+    
+    #region DELETE
+    [HttpDelete("expenses")]
+    public IActionResult DeleteExpenses()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
